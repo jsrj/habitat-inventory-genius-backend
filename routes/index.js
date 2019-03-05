c3a7c06ea2ae19660018f9a91956aa55f31346e4
Arjay Jones <robert.jones@arjaycodes.com>
2019-03-04 17:24:07 -0800

    changed routes/ to controller/

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-
-// GET Root endpoint (hateOAS root)
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-module.exports = router;
